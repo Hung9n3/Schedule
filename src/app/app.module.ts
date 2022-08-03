f1cb270abe77d5f4b9dabb131b96871012b1f62d
Hung9n3 <chovameo352@gmail.com>
2022-08-03 12:06:00 +0700

    testing more than one event in the same period

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { registerLocaleData } from '@angular/common';
-import localeZh from '@angular/common/locales/zh';
-registerLocaleData(localeZh);
+import localeVi from '@angular/common/locales/vi';
+registerLocaleData(localeVi);
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  { provide:  LOCALE_ID, useValue:'vi-Vi'}
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,31 +1,162 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnInit, ViewChild } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
 import { CalendarComponent } from 'ionic2-calendar';
+import { CalModalPage } from '../pages/cal-modal/cal-modal.page';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit{
-
+export class Tab2Page implements OnInit {
   eventSource = [];
-  viewTitle:string;
-   calendar = {
+  viewTitle: string;
+  start1 = new Date(2022, 8, 4, 8, 0, 0, 0)
+  start2 = new Date(2022,8,4,10,0,0,0)
+  end1 = new Date(2022, 8,5,12,0,0,0)
+  end2 = new Date(2022,8,7,14,0,0,0)
+  calendar = {
     mode: 'month',
-    currentDate: new Date()
-   }
-   selectedDate: Date
+    currentDate: new Date(),
+  };
 
-   @ViewChild(CalendarComponent) myCal: CalendarComponent;
-    
-  constructor() {}
+  @ViewChild(CalendarComponent) myCal: CalendarComponent;
+
+  constructor(
+    private alertCtrl: AlertController,
+    @Inject(LOCALE_ID) private locale: string,
+    private modalCtrl: ModalController
+  ) {}
   ngOnInit(): void {
+    this.eventSource.push({title: 'event1',
+    startTime: this.start1,
+    endTime: this.end1,
+    allDay: false,})
+    this.eventSource.push({title: 'Event2',
+    startTime: this.start2,
+    endTime: this.end2,
+    allDay: false,})
+    console.log(this.eventSource)
   }
-  next(){
+  next() {
     this.myCal.slideNext();
   }
-  back(){
+  back() {
     this.myCal.slidePrev();
   }
-
+  changeTitle(title) {
+    this.viewTitle = title;
+  }
+  async onEventSelected(event) {
+    // Use Angular date pipe for conversion
+    let start = formatDate(event.startTime, 'medium', this.locale);
+    let end = formatDate(event.endTime, 'medium', this.locale);
+ 
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'From: ' + start + '<br><br>To: ' + end,
+      buttons: ['OK'],
+    });
+    alert.present();
+  }
+  
+  createRandomEvents() {
+    var events = [];
+    for (var i = 0; i < 50; i += 1) {
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + startDay
+          )
+        );
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + endDay
+          )
+        );
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true,
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + startDay,
+          0,
+          date.getMinutes() + startMinute
+        );
+        endTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + endDay,
+          0,
+          date.getMinutes() + endMinute
+        );
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false,
+        });
+      }
+    }
+    this.eventSource = events;
+  }
+  removeEvents() {
+    this.eventSource = [];
+  }
+  async openCalModal() {
+    const modal = await this.modalCtrl.create({
+      component: CalModalPage,
+      cssClass: 'cal-modal',
+      backdropDismiss: false
+    });
+   
+    await modal.present();
+   
+    modal.onDidDismiss().then((result) => {
+      if (result.data && result.data.event) {
+        let event = result.data.event;
+        if (event.allDay) {
+          let start = event.startTime;
+          event.startTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate()
+            )
+          );
+          event.endTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate() + 1
+            )
+          );
+        }
+        this.eventSource.push(result.data.event);
+        this.myCal.loadEvents();
+      }
+    });
+  }
 }
